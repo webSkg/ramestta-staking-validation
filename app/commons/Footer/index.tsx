4cd9974148ee8a81d4231916aee7ce0f6dbb01ec
webSkg <sudhanshu.it@ramaera.in>
2023-10-21 13:10:09 +0530

    first commit

---
+++ app/commons/Footer/index.tsx
@@ -0,0 +1,7 @@
+import { Box, Button, Typography } from "@mui/material";
+import AllInclusiveIcon from "@mui/icons-material/AllInclusive";
+import Link from "next/link";
+const index = () => {
+  return <>footer</>;
+};
+export default index;
