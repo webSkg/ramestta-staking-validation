ea84d0eb6ce23cf570417b1c49230cea6f78a3ee
Sudhanshu Kumar Dwivedi <sudhanshu.it@ramaera.in>
2023-10-25 18:19:28 +0530

    hero

--- app/Homepage/Hero/index.tsx
+++ app/Homepage/Hero/index.tsx
@@ -1,7 +1,19 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 
 const index = () => {
-  return <Box>index</Box>;
+  return (
+    <Box height={"400px"} bgcolor={"#0C111F"} display={"flex"}>
+      <Box width={"50%"}>
+        <Typography color={"white"} fontSize={42} marginY={"auto"}>
+          Start earning rewards with{" "}
+          <span style={{ color: "#00796D" }}>Rama Staking.</span>
+        </Typography>
+      </Box>
+      <Box width={"fit-content"}>
+        <img src="/assets/coin.png" alt="" />
+      </Box>
+    </Box>
+  );
 };
 
 export default index;
